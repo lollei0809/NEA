a94dcd5f868033976e05e5d7c78080a0235fe137
lollei0809 <lollei0809@highgateschool.org.uk>
2024-09-28 20:04:22 +0100

    added testing

--- questions.py
+++ questions.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 
 class Question(ABC):  # abstract base class which other question objects inherit from
     def __init__(self):
-        self.plausible_answers = ["", "", ""]
+        self.plausible_answers = []
         self.correct_answer = ""
         self.user_answer = ""
         self.question = ""
@@ -37,8 +37,9 @@ class Question(ABC):  # abstract base class which other question objects inherit
         self.wrongly_answered += 1
 
 
-class Unsigned_question(Question):
+class UnsignedQuestion(Question):
     def __init__(self):
+        super().__init__()  # Call the parent constructor
         self.types = []
         self.num_bits = 8
 
@@ -50,19 +51,20 @@ class Unsigned_question(Question):
     def generate_question(self):
         bits = ""
         if self.question_type == self.types[0]:
-            self.question = random.randint(0, 2**self.num_bits-1)
+            self.question = random.randint(0, 2 ** self.num_bits - 1)
         else:
             for i in range(self.num_bits):
-                bits = bits + (random.choice([0, 1]))
+                bits = bits + str(random.choice([0, 1]))
             self.question = bits
 
     def generate_correct_answer(self):
-        if self.question_type == self.question_type[1]:
-            j = 2**self.num_bits
+        self.correct_answer = ""
+        if self.question_type == self.types[0]:
+            j = 2 ** (self.num_bits-1)
             total = ""
             temp = int(self.question)
             for i in range(self.num_bits):
-                if temp > j:
+                if temp >= j:
                     total += "1"
                     temp -= j
                 else:
@@ -74,31 +76,50 @@ class Unsigned_question(Question):
             j = 1
             total = 0
             for i in range(self.num_bits):
-                total += j * int(self.question [-(i + 1)])
+                total += j * int(self.question[-(i + 1)])
                 j *= 2
-            self.correct_answer = str(total)
+            self.correct_answer = total
         self.plausible_answers.append(self.correct_answer)
 
     def generate_plausible_answers(self):
-        if self.question_type == self.question_type[1]:
+        if self.question_type == self.types[0]:
+            bit_index = -1
             for i in range(2):
-                bit_index = random.randint(0,self.num_bits-1)
-                temp = self.correct_answer                 #passing mutable objects calls by reference?
-                if temp[bit_index] == 0:
-                    temp[bit_index] = 1
+                exclude = [bit_index]
+                while True:
+                    bit_index = random.randint(0, self.num_bits - 1)
+                    if bit_index not in exclude: # excludes the number that has already been used so plausible answers
+                        # doesnt have duplicates
+                        break
+                temp = list(self.correct_answer)  # passing mutable objects calls by reference?
+                if temp[bit_index] == "0":
+                    temp[bit_index] = "1"
                 else:
-                    temp[bit_index] = 0
-                self.plausible_answers.append(temp)
-        else:
+                    temp[bit_index] = "0"
+                t = "".join(temp)
+                self.plausible_answers.append(t)
+        else:# all odd or all even so more dificult
+            num = -1*(int(self.correct_answer)+1)
             for i in range(2):
-                num = random.randint(-1*int(self.correct_answer),int(self.correct_answer))
-                temp = self.correct_answer+num
+                exclude=[num]
+                while True:
+                    num = random.randint(-1 * int(self.correct_answer), int(self.correct_answer))
+                    if num not in exclude:
+                        break
+                temp = int(self.correct_answer) + num
                 self.plausible_answers.append(temp)
         random.shuffle(self.plausible_answers)
 
+
 if __name__ == "__main__":  # importing this module wont run the code
-    question = Unsigned_question()
+    # put these tests in pytest file
+    #create new subclasses after tests are written: test driven development
+    question = UnsignedQuestion()
     question.generate_question_type()
     question.generate_question()
+    print("Question:", question.question)
     question.generate_correct_answer()
+    print("Correct Answer:", question.correct_answer)
     question.generate_plausible_answers()
+    print("Plausible Answers:", question.plausible_answers)
+    print(question.question_type)
---
+++ test_questions.py
@@ -0,0 +1,27 @@
+import pytest
+from questions import UnsignedQuestion
+
+
+class TestUnsignedQuestion:
+    @pytest.fixture
+    def unsigned_question(self):
+        test_question = UnsignedQuestion()
+        test_question.generate_question_type()
+        test_question.generate_question()
+        test_question.generate_correct_answer()
+        test_question.generate_plausible_answers()
+        return test_question
+
+    def test_UQ_setup(self, unsigned_question):
+        assert unsigned_question.num_bits == 8
+
+    def test_generate_question_type(self, unsigned_question):
+        types = [f"convert this denery number {self.question} to unsigned binary",
+                 f"convert this unsigned binary number {self.question} to denery"]
+        assert unsigned_question.question_type in types
+
+    def test_question(self, unsigned_question):
+        if unsigned_question.question_type == type[0]:
+            assert unsigned_question.question >= 0 and unsigned_question <= 255
+        if unsigned_question.question_type == type[1]:
+            assert True
